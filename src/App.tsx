bea4d0d538462a04219696aa08221cb14235e5b9
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2024-01-01 09:35:45 +0330

    fixed  a bug.

--- src/App.tsx
+++ src/App.tsx
@@ -426,7 +426,7 @@ function App() {
                 <GlobalAppletSheet />
                 {
                     overlaySafezone ? (
-                        <div style={{ position: 'fixed', left: 0, top: 0, zIndex: 99999 }}>
+                        <div style={{ width: '100%', height: '100%', position: 'fixed', left: 0, top: 0, zIndex: 99999 }}>
                             <Safezone code={overlaySafezone.code} workerId={overlaySafezone.workerId} roomId={overlaySafezone.room.id} towerId={overlaySafezone.room.towerId} />
                         </div>
                     ) : null
