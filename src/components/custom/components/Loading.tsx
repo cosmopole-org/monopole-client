7ccdada7c62a4c8fc6a1a690b978495c54c95761
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2024-01-06 14:42:27 +0330

    fixed bugs.

--- src/components/custom/components/Loading.tsx
+++ src/components/custom/components/Loading.tsx
@@ -42,23 +42,35 @@ const Loading = (props: { onCancel: () => void, overlay?: boolean, isWidget?: bo
                 ) : (
                     <div style={{ position: 'absolute', left: '50%', top: 'calc(50% - 16px)', transform: 'translate(-50%, -50%)' }}>
                         <div style={{ width: 'auto', height: 'auto', position: 'relative', padding: 32 }}>
-                            <Paper style={{ width: 56, height: 56, borderRadius: '50%', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
-                                <CircularProgress style={{ width: '80%', height: '80%', margin: '10%' }} variant="indeterminate" />
-                            </Paper>
-                            <div style={{ width: 56, height: 56 }} />
-                            <Typography variant="body1" style={{ width: '100%', textAlign: 'center', marginTop: 12 }}>
-                                Loading Applet...
-                            </Typography>
-                            <SigmaFab
-                                style={{ marginTop: 32 }}
-                                onClick={() => {
-                                    props.onCancel()
-                                }}
-                                variant="extended"
-                            >
-                                <Close style={{ fill: themeBasedTextColor.get({ noproxy: true }), marginRight: 12 }} />
-                                Cancel
-                            </SigmaFab>
+                            {
+                                props.isWidget ? (
+                                    <div style={{ width: 'auto', height: 'auto', position: 'relative', padding: 32 }}>
+                                        <Paper style={{ width: 56, height: 56, borderRadius: '50%', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
+                                            <CircularProgress style={{ width: '80%', height: '80%', margin: '10%' }} variant="indeterminate" />
+                                        </Paper>
+                                    </div>
+                                ) : (
+                                    <div style={{ width: 'auto', height: 'auto', position: 'relative', padding: 32 }}>
+                                        <Paper style={{ width: 56, height: 56, borderRadius: '50%', position: 'absolute', left: '50%', transform: 'translateX(-50%)' }}>
+                                            <CircularProgress style={{ width: '80%', height: '80%', margin: '10%' }} variant="indeterminate" />
+                                        </Paper>
+                                        <div style={{ width: 56, height: 56 }} />
+                                        <Typography variant="body1" style={{ width: '100%', textAlign: 'center', marginTop: 12 }}>
+                                            Loading Applet...
+                                        </Typography>
+                                        <SigmaFab
+                                            style={{ marginTop: 32 }}
+                                            onClick={() => {
+                                                props.onCancel()
+                                            }}
+                                            variant="extended"
+                                        >
+                                            <Close style={{ fill: themeBasedTextColor.get({ noproxy: true }), marginRight: 12 }} />
+                                            Cancel
+                                        </SigmaFab>
+                                    </div>
+                                )
+                            }
                         </div>
                     </div>
                 )
